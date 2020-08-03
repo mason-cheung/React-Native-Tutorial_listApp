c8fc65fb9447d0a5ecfee9848f248a4edd2cf4d8
masonAIT <masoncch@users.noreply.github.com>
2020-08-03 18:50:09 +1000

    Second commit

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView, FlatList } from 'react-native';
 import { Component } from 'react';
 
+import {Item} from './components/Item';
+
 export default class App extends Component {
+  listData = [
+    { id: '1', amount: 50, category: 'food'},
+    { id: '2', amount: 150, category: 'groceries'},
+    { id: '3', amount: 30, category: 'transport'},
+  ]
   render() {
     return (
       <SafeAreaView>
-        
+        <FlatList 
+          data={this.listData}
+          renderItem={this.renderList}
+          keyExtractor={ expense => expense.id }
+        />
       </SafeAreaView>
     )
   }
+  renderList = ({item}) => (
+    <Item amount={item.amount} category={item.category} />
+  )
 }
 
 const styles = StyleSheet.create({
